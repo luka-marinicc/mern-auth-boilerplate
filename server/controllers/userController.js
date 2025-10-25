e00607f3a4a0ce537bb7d76f317f60c5c3152d9d
Prlekk <marinic.sola@gmail.com>
2025-10-25 17:52:44 +0200

    chore(server): Fixed User.js import in userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,4 @@
-import User from "../models/User";
+import User from "../models/User.js";
 
 export const getMe = async (req, res) => {
     const user = await User.findById(req.user._id).select("-password");
